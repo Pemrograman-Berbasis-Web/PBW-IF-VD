0fa83ea257b458ee13dcd5e59c662cd1e7734af6
AuraRa-chan <220660121094@student.unsap.ac.id>
2024-12-09 13:41:07 +0700

    menambahkan Portofolio

---
+++ 220660121094/Portofolio/code/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portofolio dengan fitur tambahan siap digunakan!");
+});
+function openResume() {
+    // Replace 'cv.pdf' with the path to your actual CV file
+    window.open('Portofolio\Assets\CV Aura Rahayu.pdf', '_blank');
+}
