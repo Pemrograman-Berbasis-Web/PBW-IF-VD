d672e16b406782c9ebd5f71becad1e390c1bfe05
AnjelinaMentariRustandi <220660121024@student.unsap,ac.id>
2025-01-17 16:24:46 +0700

    mengganti tugas laravel

--- 220660121024/Framework Laravel/Framework Laravel/Framework/webpack.mix.js
+++
@@ -1,9 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/assets/js')
-    .sass('resources/sass/app.scss', 'public/assets/css')
-    .copy('resources/images/', 'public/assets/images')
-    .options({
-        processCssUrls: false
-    })
-    .version();
